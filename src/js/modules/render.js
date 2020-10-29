e3862a1325cb9b692cf64428ec7d2ac4796fcb52
Владислав <palivoda.v.wezom@gmail.com>
2020-10-29 10:50:43 +0200

    дописывает функцию рендера

--- src/js/modules/render.js
+++ src/js/modules/render.js
@@ -1,15 +1,30 @@
 export const render = (template, data, place, config) => {
-    const $template = $($(template)[0].content);
-    console.log($template)
-    const buildMarkup = (maxSize) => {
-        const markup = '';
+    const buildMarkup = (data, maxSize) => {
+        const $template = $($(template)[0].content.children);
+        let markup = '';
 
         data.forEach((item) => {
-            let newTemplateInstance = $template.clone();
-            newTemplateInstance.find('.card__brand').text(data.brand.name);
+            let newTemplateInstance = $template.clone(true);
+            newTemplateInstance.find('[data-brand]').text(item.brand.name);
+            newTemplateInstance.find('[data-image]').attr('src', item.image.sizes['card-preview']).attr('alt', item.image.alt);
+            newTemplateInstance.find('[data-manufacturer]').text(item.manufacturer.name);
+            newTemplateInstance.find('[data-year]').text(item.year);
+            newTemplateInstance.find('[data-model]').text(item.model.name);
+            newTemplateInstance.find('[data-price]').text(`${item.price.currency.symbol}${item.price.value}`);
+
+            markup += newTemplateInstance.get(0).outerHTML
         })
 
+        return markup;
     }
 
-    buildMarkup()
+    const insertMarkup = (place) => {
+        $(place).html(buildMarkup(data))
+    }
+
+    const init = () => {
+        insertMarkup(place);
+    }
+
+    init();
 }
